969cd1de77d554401c97b1bbba57c25bdbd9620f
Richard Johansson <richard@newsdesk.se>
2011-09-09 14:00:29 +0200

    Added reboot capabilities to selfupdate command

--- lib/commands/selfupdate.rb
+++ lib/commands/selfupdate.rb
@@ -1,19 +1,25 @@
 class Selfupdate < Flower::Command
   respond_to "selfupdate"
   def self.respond(command, message, sender, flower)
-    system("git fetch")
-    diff = `git log --decorate --left-right --graph --cherry-pick --oneline master...origin/master`
-    if diff.empty?
-      flower.say("No updates found. No need to reboot!")
-    else
-      flower.say("Applying updates and rebooting:")
-      flower.paste(diff)
-      system("git pull --rebase")
+    if message == "reboot"
+      flower.say("Rebooting...")
       exec("rake run &")
+    else
+      system("git fetch")
+      diff = `git log --decorate --left-right --graph --cherry-pick --oneline master...origin/master`
+
+      if diff.empty?
+        flower.say("No updates found. No need to reboot!")
+      else
+        flower.say("Applying updates and rebooting:")
+        flower.paste(diff)
+        system("git pull --rebase")
+        exec("rake run &")
+      end
     end
   end
   
   def self.description
-    "Fetch latest version and restart Flower"
+    "Update bot firmware. Pass 'reboot' to reboot only."
   end
 end
