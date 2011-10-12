8dbc26395d2ad1d1166bbe7ef00256b369eed1f2
Jonas Forsberg <himynameisjonas@gmail.com>
2011-10-12 20:16:21 +0200

    Added sound fx commands

---
+++ lib/commands/sound_fx.rb
@@ -0,0 +1,42 @@
+class SoundFx < Flower::Command
+  respond_to "easy", "friday", "rimshot", "sad", "yeah"
+
+  def self.description
+    "Awesome audio fx!"
+  end
+
+  def self.respond(command, message, sender, flower)
+    case command
+    when "easy"
+      Spotify.lower_spotify do
+        play_file "easy.mp3"
+      end
+    when "friday"
+      if Time.now.wday == 5
+        Spotify.lower_spotify do
+          play_file("friday.mp3")
+        end
+      else
+        flower.say("Today != rebeccablack", :mention => sender[:id])
+      end
+    when "rimshot"
+      Spotify.lower_spotify do
+        play_file "rimshot.mp3"
+      end
+    when "sad"
+      Spotify.lower_spotify do
+        play_file "sadtrombone.mp3"
+      end
+    when "yeah"
+      Spotify.lower_spotify do
+        play_file "yeah.mp3"
+      end
+    end
+  end
+
+  private
+
+  def self.play_file(file_name)
+    system "afplay", File.expand_path(File.join(__FILE__, "..", "..", "..", "extras", file_name))
+  end
+end
