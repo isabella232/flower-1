e0db735736efd96a53d618c28868c473843785cf
Jonas Forsberg <himynameisjonas@gmail.com>
2011-10-27 19:09:15 +0200

    fixed url for pictures not ending in jpg/png/gif

--- lib/commands/pic.rb
+++ lib/commands/pic.rb
@@ -1,5 +1,5 @@
 class Pic < Flower::Command
-  respond_to "pic"
+  respond_to "pic", "picture", "image"
   require 'open-uri'
   require 'json'
   require 'cgi'
@@ -22,6 +22,7 @@ class Pic < Flower::Command
   def self.search_bing(query)
     url = "http://api.search.live.net/json.aspx?AppId=#{Flower::Config.live_id}&Query=#{CGI.escape "\"#{query}\""}&Sources=Image&Version=2.0&Adult=Moderate&Image.Count=1"
     json = JSON.parse(open(url).read)
-    json["SearchResponse"]["Image"]["Results"][0]["MediaUrl"]
+    image_url = json["SearchResponse"]["Image"]["Results"][0]["MediaUrl"]
+    image_url.match(/(jpg|png|gif)$/) ? image_url : image_url + "#.jpg"
   end
 end
