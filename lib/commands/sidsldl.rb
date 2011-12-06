9c094bff552f2e2bd1bd303b864fc08facc9676a
Jonas Forsberg <himynameisjonas@gmail.com>
2011-12-06 15:03:55 +0100

    Sidsldl!

---
+++ lib/commands/sidsldl.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+class Sidsldl < Flower::Command
+  listen_to /varför.*\?/i
+  
+  def self.description
+    "Skit i det så lever du längre"
+  end
+
+  def self.listen(message, sender, flower)
+    flower.say("Skit i det #{sender[:nick]} så lever du längre!", :mention => sender[:id])
+  end
+end
