9bcc1a6e5258bae241f1e693eeb61923f659b03e
Jonas Forsberg <himynameisjonas@gmail.com>
2011-12-06 14:25:49 +0100

    added random pic command

--- lib/commands/pic.rb
+++ lib/commands/pic.rb
@@ -1,5 +1,5 @@
 class Pic < Flower::Command
-  respond_to "pic", "picture", "image"
+  respond_to "pic", "picture", "image", "rpic"
   require 'open-uri'
   require 'json'
   require 'cgi'
@@ -12,17 +12,27 @@ class Pic < Flower::Command
 
   def self.respond(command, message, sender, flower)
     begin
-      image = IMAGE_CACHE.has_key?(message) ? IMAGE_CACHE[message] : search_bing(message)
-      flower.say(image)
+      if command == "rpic"
+        image = search_bing(message, true)
+        flower.say(image)
+      else
+        image = IMAGE_CACHE.has_key?(message) ? IMAGE_CACHE[message] : search_bing(message)
+        flower.say(image)
+      end
     rescue NoMethodError
       flower.say("sorry, no matches")
     end
   end
 
-  def self.search_bing(query)
-    url = "http://api.search.live.net/json.aspx?AppId=#{Flower::Config.live_id}&Query=#{CGI.escape "\"#{query}\""}&Sources=Image&Version=2.0&Adult=Moderate&Image.Count=1"
+  def self.search_bing(query, random = false)
+    limit = random ? 50 : 1
+    url = "http://api.search.live.net/json.aspx?AppId=#{Flower::Config.live_id}&Query=#{CGI.escape "\"#{query}\""}&Sources=Image&Version=2.0&Adult=Moderate&Image.Count=#{limit}"
     json = JSON.parse(open(url).read)
-    image_url = json["SearchResponse"]["Image"]["Results"][0]["MediaUrl"]
+    if random
+      image_url = json["SearchResponse"]["Image"]["Results"].shuffle[0]["MediaUrl"]
+    else
+      image_url = json["SearchResponse"]["Image"]["Results"][0]["MediaUrl"]
+    end
     image_url.match(/(jpg|png|gif)$/) ? image_url : image_url + "#.jpg"
   end
 end
