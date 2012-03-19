9f56d5329f9b2fa9b815d6e2afa0f85758a51da7
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-03-19 11:36:14 +0100

    Add Erik-commandot

---
+++ lib/commands/erik.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+class Erik < Flower::Command
+  listen_to /erik.*(deploy|deploya|kaos|deadline|möte)|(deploy|deploya|kaos|deadline|möte).*erik/i
+
+  def self.description
+    "Erik punchlines"
+  end
+
+  def self.listen(message, sender, flower)
+    flower.say("#{sender[:nick]}, " + line[rand(line.size)], :mention => sender[:id])
+  end
+
+  def self.line
+    [
+      'jag kanske bara kaosar här, men...',
+      'githubba gemsarna!',
+      'tryck upp den rakt upp i mastern!',
+      'vi kan ta ett möte om det senare.',
+      'jag kommer in vid kl 10, vi kan väl ta det därefter?'
+    ]
+  end
+end
+
+# 
+
