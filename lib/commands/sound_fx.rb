38097a15c8e32d087ecf62b9ca735927be2d84d0
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-09-10 09:42:42 +0200

    Added keke commands

--- lib/commands/sound_fx.rb
+++ lib/commands/sound_fx.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require_relative 'sound_command'
 class SoundFx < SoundCommand
-  respond_to "easy", "rimshot", "sad", "yeah", "applause", "bomb", "suprise", "snore", "godwillsit", "sting", "khan", "tarelugn", "tadetlugnt"
+  respond_to "easy", "rimshot", "sad", "yeah", "applause", "bomb", "suprise", "snore", "godwillsit", "sting", "pengar", "rik", "fel", "khan", "tarelugn", "tadetlugnt"
 
   def self.description
     "Awesome audio fx!"
@@ -27,6 +27,12 @@ class SoundFx < SoundCommand
       play_file "snore.wav"
     when "godwillsit"
       play_file "godwillsit.m4a"
+    when "pengar"
+      play_file "pengar.mp3"
+    when "rik"
+      play_file "rik.mp3"
+    when "fel"
+      play_file "fel.mp3"
     when "sting"
       play_file "sting.wav"
     when "khan"
