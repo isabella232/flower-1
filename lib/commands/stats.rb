3b741e95cf39cb8f4b42ce428d39c2d009b279ac
Jonas Forsberg <himynameisjonas@gmail.com>
2012-09-10 15:21:43 +0200

    Prettify stats output

--- lib/commands/stats.rb
+++ lib/commands/stats.rb
@@ -7,15 +7,13 @@ class Stats < Flower::Command
     message_array = message.split(" ")
     case message_array.first
     when "online"
-      flower.say("Online right now: #{online_right_now}")
+      flower.paste ["Online right now: #{online_right_now}"]
     when "commands"
       nick = message_array[1] || sender[:nick]
-      flower.say "Commands stats for #{nick}"
-      flower.paste command_stats_for(nick)
+      flower.paste ["Stats for #{nick}"] << command_stats_for(nick)
     when "sax"
       nick = message_array[1] || sender[:nick]
-      flower.say "Sax stats for #{nick}"
-      flower.paste sax_stats_for(nick)
+      flower.paste ["Sax stats for #{nick}"] << sax_stats_for(nick)
     else
       flower.say("Online right now: #{online_right_now}")
     end
@@ -28,12 +26,12 @@ class Stats < Flower::Command
   private
 
   def self.command_stats_for(nick)
-    stats = Flower::Stats.find("commands/#{nick.downcase}", 1.hours.ago, 1.hour.from_now).total.reject{|v| v == "!"}
+    stats = Flower::Stats.find("commands/#{nick.downcase}", 12.hours.ago, 12.hour.from_now).total.reject{|v| v == "!"}
     stats.map {|type, value| "#{type}: #{value}"} << "totalt: #{stats.values.inject(:+) || 0}"
   end
 
   def self.sax_stats_for(nick)
-    stats = Flower::Stats.find("sax/#{nick.downcase}", 1.hours.ago, 1.hour.from_now).total
+    stats = Flower::Stats.find("sax/#{nick.downcase}", 12.hours.ago, 12.hour.from_now).total
     stats.map {|type, value| "#{type}: #{value}"} << "totalt: #{stats.values.inject(:+) || 0}"
   end
 
