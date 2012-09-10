e349388d10c0e3b0bba6ba9e4cab7dc2830b4293
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-09-10 10:57:40 +0200

    Updated help command, less is more

--- lib/commands/help.rb
+++ lib/commands/help.rb
@@ -4,8 +4,12 @@ class Help < Flower::Command
   respond_to "help", "hjälp", "?"
 
   def self.respond(command, message, sender, flower)
-    flower.say("Available commands:")
-    flower.paste(help_text)
+    if message == ""
+      flower.say("Available commands:")
+      flower.paste(help_text)
+    else
+      flower.paste(help_command(message) || "nothing found")
+    end
   end
 
   def self.description
@@ -24,9 +28,13 @@ class Help < Flower::Command
 
     text_array = []
     command_hash.reject{ |_, c| c.nil? || c.empty? }.each do |klass, command|
-      text_array << command.ljust(longest_command.length + 5) +
-        "#{"#{klass.description}" if klass.respond_to?(:description)}"
+      text_array << command.ljust(longest_command.length + 5)
     end
     text_array.sort
+  end
+
+  def self.help_command(command)
+    Flower::COMMANDS[command].description if Flower::COMMANDS.has_key? command
+    #    "#{"#{klass.description}" if klass.respond_to?(:description)}"
   end
 end
