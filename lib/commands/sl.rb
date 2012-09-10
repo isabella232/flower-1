b769724eca74107d1a87eabbaa7c3e8ecc48845d
Joakim Westerlund <joakim.westerlund@gmail.com>
2012-09-10 13:58:49 +0200

    Added command for SL - Södra Station, Buss/Pendel

---
+++ lib/commands/sl.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+require 'open-uri'
+class SL < Flower::Command
+  respond_to 'sl'
+
+  class << self
+    def description
+      'SL from Stockholm Södra'
+    end
+
+    def respond(command, message, sender, flower)
+      flower.paste(soder, :mention => sender[:id])
+    end
+
+    private
+    def html
+      open('http://realtid.sl.se/?id=177&epslanguage=sv&WbSgnMdl=1365-U8O2ZHJhIHN0YXRpb24gKHDDpSBSb3Nlbmx1bmRzZykgKFN0b2NraG9sbSk%3d-_--_-_-_').read
+    end
+
+    def soder
+      trs = Nokogiri::HTML.parse(html).css('div.trafficList tr.item')
+
+      trs.map do |tr|
+        tr.css("td").map do |td|
+          td.text.gsub(/\t|\n|\r/,"")
+        end.join(" ")
+      end
+    end
+  end
+end
