f10a0dea2451c965c949b4f248cd255499074ac6
Martin Svalin <martin@lite.nu>
2012-09-12 11:14:31 +0200

    Trailing whitespace

--- lib/config.rb
+++ lib/config.rb
@@ -1,6 +1,6 @@
 class Flower::Config
   CONFIG = YAML.load File.read("config.yml")
-  
+
   def self.method_missing(sym, *args, &block)
     CONFIG[sym.to_s]
   end
--- lib/local_server.rb
+++ lib/local_server.rb
@@ -2,7 +2,7 @@ class LocalServer < EventMachine::Connection
 	attr_accessor :flower
 
 	def set_flower(f)
-		@flower = f	
+		@flower = f
 	end
 
 	def receive_data(data)
