e869f9ff958e0af1ed16d1eb63002142cadaee65
Emil Löfquist <emil.lofquist@newsdesk.se>
2012-10-01 16:37:12 +0200

    Expose classes that inherits from SoundCommand

--- lib/commands/sound_command.rb
+++ lib/commands/sound_command.rb
@@ -1,6 +1,10 @@
 # encoding: UTF-8
 class SoundCommand < Flower::Command
 
+  def self.subclasses
+      ObjectSpace.each_object(Class).select { |klass| klass < self }
+  end
+
   private
 
   def self.play_file(file_name, lower_spotify = true)
