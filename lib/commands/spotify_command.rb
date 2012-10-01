d17cd1c0052167bb832ee5e6a04959862f8a1d74
Richard Johansson <richard.johansson@gmail.com>
2012-10-01 20:23:21 +0200

    First version of The Queue

--- lib/commands/spotify_command.rb
+++ lib/commands/spotify_command.rb
@@ -1,8 +1,14 @@
 class SpotifyCommand < Flower::Command
-  respond_to "play", "pause", "track", "stam", "search"
+  respond_to "play", "pause", "track", "stam", "search", "queue"
   require 'appscript'
   require 'hallon'
   require 'hallon-openal'
+
+  QUEUE = []
+
+  class << self
+    attr_accessor :hallon_track
+  end
 
   def self.respond(command, message, sender, flower)
     case command
@@ -16,31 +22,60 @@ class SpotifyCommand < Flower::Command
     when "track"
       flower.say(get_current_track)
     when "search"
-      response = search(message)
+      response = search_tracks(message)
       flower.paste(response)
+    when "queue"
+      case message.split(" ").first
+      when nil
+        if QUEUE.empty?
+          flower.say("Queue is empty.")
+        else
+          flower.paste(["Next in queue (#{QUEUE.size})", QUEUE[0, 5].map{ |track| "#{track.artist.name} - #{track.name}" }])
+        end
+      else
+        track = find_track(message)
+        QUEUE << track
+        flower.say "Added to queue (#{QUEUE.size}): #{track.artist.name} - #{track.name}"
+      end
     when "play"
       case message.split(" ").first
       when nil
-        spotify.play
+        play_current
       when "next"
-        player.pause
-        spotify.play
-        spotify.next_track
-        flower.say(get_current_track)
+        play_next
       when "prev"
         spotify.previous_track
         sleep 0.2
         spotify.previous_track
-        flower.say(get_current_track)
       else
-        song = play_song(message)
-        flower.say "Playing: #{song}"
+        track = find_track(message)
+        play_track(track)
       end
+      flower.say(get_current_track)
+    end
+  end
+
+  def self.play_current
+    if hallon_track
+      player.play
+    else
+      spotify.play
+    end
+  end
+
+  def self.play_next
+    player.pause
+    if track = QUEUE.pop
+      play_track(track)
+    else
+      hallon_track = nil
+      spotify.play
+      spotify.next_track
     end
   end
 
   def self.description
-    "Spotify: \\\"play next/prev/query\\\", \\\"pause\\\", \\\"track\\\""
+    "Spotify: \\\"play next/prev\\\", \\\"pause\\\", \\\"track\\\", \\\"search\\\", \\\"queue\\\""
   end
 
   def self.lower_spotify
@@ -61,44 +96,53 @@ class SpotifyCommand < Flower::Command
     Appscript::app("Spotify").sound_volume.set volume
   end
 
-  def self.get_current_track
-    "Playing: #{spotify.current_track.artist.get} - #{spotify.current_track.name.get}"
-  end
-
-  def self.play_song(query)
-    if query.match(/^spotify:/)
-      track = Hallon::Track.new(query).load
-    else
-      track = search_for(query).first
-    end
+  def self.play_track(track)
     spotify.pause
+    hallon_track = track
     Thread.new do
       player.play!(track)
-      spotify.play
+      play_next
     end
-    "#{track.artist.name} - #{track.name}"
+  end
+
+  def self.get_current_track
+    "Playing: ".tap do |message|
+      if hallon_track
+        message << "#{hallon_track.artist.name} - #{hallon_track.name}"
+      else
+        message << "#{spotify.current_track.artist.get} - #{spotify.current_track.name.get}"
+      end
+    end
+  end
+
+  def self.find_track(query)
+    if query.match(/^spotify:/)
+      Hallon::Track.new(query).load
+    else
+      search(query).first
+    end
+  end
+
+  def self.search_tracks(query)
+    result = search(query)
+    response = ["Found #{result.size} tracks:"]
+    response << result[0, 8].map{ |track| "#{track.artist.name} - #{track.name} (#{track.to_link.to_uri})" }
   end
 
   def self.search(query)
-    result = search_for(query)
-    response = ["Found #{result.size} tracks:"]
-    response << result[0, 5].map{ |track| "#{track.artist.name} - #{track.name} (#{track.to_link.to_uri})" }
-  end
-
-  def self.search_for(query)
     Hallon::Search.new(query).load.tracks
   end
 
   def self.player
-    @player ||= Hallon::Player.new(Hallon::OpenAL)
+    @@player ||= Hallon::Player.new(Hallon::OpenAL)
   end
 
   def self.spotify
-    @spotify ||= Appscript::app("Spotify")
+    @@spotify ||= Appscript::app("Spotify")
   end
 
   def self.init_session
-    @hallon_session ||= hallon_session!
+    @@hallon_session ||= hallon_session!
   end
 
   def self.hallon_session!
