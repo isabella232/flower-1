e8cdf3b92017a46c32fdedd9f50e1d94df6f9f3a
Markus Nordin <markus.nordin@gmail.com>
2012-10-15 15:22:07 +0200

    They took our jobs!

---
+++ lib/commands/job.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+require_relative 'sound_command'
+class Job < SoundCommand
+  respond_to "job"
+
+  FILES = Dir.glob("extras/jobs/*.mp3").map{|f| f.gsub("extras/","")}
+
+  def self.description
+    "They took our jobs!"
+  end
+
+  def self.respond(command, message, sender, flower)
+    play_file FILES.sample
+  end
+end
