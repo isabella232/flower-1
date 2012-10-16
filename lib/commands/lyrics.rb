384be5e5ca24d910eb0b9aa505e15770caf4207a
Richard Johansson <richard.johansson@gmail.com>
2012-10-16 14:08:28 +0200

    Get current song by default in !lyrics

--- lib/commands/lyrics.rb
+++ lib/commands/lyrics.rb
@@ -7,8 +7,12 @@ class Lyrics < Flower::Command
   URL = "http://search.letssingit.com/cgi-exe/am.cgi?a=search&l=archive&s=<q>"
 
   def self.respond(command, message, sender, flower)
-    lyrics = find_lyrics(message)
-    flower.paste(lyrics)
+    message = get_current_song unless message.present?
+    if lyrics = find_lyrics(message)
+      flower.paste(lyrics)
+    else
+      flower.say("Could not find song lyrics. :(")
+    end
   end
 
   def self.find_lyrics(query)
@@ -16,5 +20,15 @@ class Lyrics < Flower::Command
     lyrics_href = document.at_css(".data_list td:nth(3) a").attribute("href").value
     document = Nokogiri.HTML(open(lyrics_href))
     document.at_css("div#lyrics").inner_html.gsub("<br>", "").split("\n")
+  rescue
+    false
+  end
+
+  def self.get_current_song
+    if SpotifyCommand.hallon_track
+      "#{SpotifyCommand.hallon_track.artist} #{SpotifyCommand.hallon_track.name}"
+    elsif SpotifyCommand.spotify.current_track
+      "#{SpotifyCommand.spotify.current_track.artist.get} #{SpotifyCommand.spotify.current_track.name.get}"
+    end
   end
 end
