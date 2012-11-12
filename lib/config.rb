3209cd29cc604000f933fc4860cea78429aa284f
Martin Svalin <martin@lite.nu>
2012-11-12 15:00:24 +0100

    Set config file on `rake run`
    Example:
        rake run CONFIG=support_config.yml

--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,5 @@
 class Flower::Config
-  CONFIG = YAML.load File.read("config.yml")
+  CONFIG = YAML.load File.read(ENV.fetch("CONFIG") {"config.yml"})
 
   def self.method_missing(sym, *args, &block)
     CONFIG[sym.to_s]
