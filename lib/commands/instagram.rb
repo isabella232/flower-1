48e3d0909dacaab24981be07dc11dc06ea71edeb
Richard Johansson <richard.johansson@gmail.com>
2012-11-12 15:16:07 +0100

    More solid Instagram error handling when entering erroneous searches

--- lib/commands/instagram.rb
+++ lib/commands/instagram.rb
@@ -11,15 +11,19 @@ class InstagramCommand < Flower::Command
   def self.respond(command, message, sender, flower)
     if message.present?
       search = URI.escape(message.split(" ").first)
-      flower.say(image_path(search))
+      if image = image_path(search)
+        flower.say(image)
+      else
+        flower.say("No hits :(")
+      end
     else
       flower.say("(Enter search term)")
     end
   end
 
   def self.image_path(query)
-    result = Instagram.tag_recent_media(query)
-    if data = result.data.to_a.try(:sample)
+    result = Instagram.tag_recent_media(query) rescue nil
+    if result && data = result.data.to_a.try(:sample)
       data.images.standard_resolution.url
     end
   end
