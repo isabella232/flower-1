c546938c879aea4cbe325de6f94a68947f600b4b
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-12-13 14:02:35 +0100

    Scarf

---
+++ lib/commands/scarf.rb
@@ -0,0 +1,19 @@
+require 'typhoeus'
+require 'nokogiri'
+class Scarf < Flower::Command
+  respond_to "scarf", "erik"
+  URL = "http://reklamaremedhalsdukinomhus.tumblr.com/random"
+
+  def self.description
+    "Reklamare med halsduk inomhus"
+  end
+
+  def self.respond(command, message, sender, flower)
+    flower.say(image)
+  end
+
+  def self.image
+    document = Nokogiri.HTML(Typhoeus::Request.get(URL, :follow_location => true).body)
+    document.at_css(".photo img").attribute('src').value
+  end
+end
