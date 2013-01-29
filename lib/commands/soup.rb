bdadf0b81c83785d4836f3fea3942397e9a3e111
Markus Nordin <markus.nordin@gmail.com>
2013-01-29 11:15:20 +0100

    Fix !soup command to fetch current menu

---
+++ lib/commands/soup.rb
@@ -0,0 +1,19 @@
+require 'typhoeus'
+require 'nokogiri'
+class Soup < Flower::Command
+  respond_to "soup"
+  URL = "http://iloapp.olandskan.se/blog/dagens?category=0"
+
+  def self.description
+    "Get soup menu for the week"
+  end
+
+  def self.respond(command, message, sender, flower)
+    flower.paste(menu)
+  end
+
+  def self.menu
+    document = Nokogiri.HTML(Typhoeus::Request.get(URL, :follow_location => true).body)
+    document.at_css("#mainDiv > .post").css("p").map(&:text).join("\n")
+  end
+end
