85e7f568da3590fc16bf8fcae4cdcdb8026312f8
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-02-01 13:57:07 +0100

    Revert "Todays soup!!"
    This reverts commit 8b15e8f55490691ad4300fd97cc7ad552ae11810.

--- lib/commands/soup.rb
+++ lib/commands/soup.rb
@@ -1,31 +1,19 @@
 require 'typhoeus'
 require 'nokogiri'
-require 'time'
-
 class Soup < Flower::Command
   respond_to "soup", "soppa"
   URL = "http://iloapp.olandskan.se/blog/dagens?category=0"
-  WDAY = Time.now.wday
 
   def self.description
     "Get soup menu for the week"
   end
 
   def self.respond(command, message, sender, flower)
-    if message == "todays" || message == "dagens"
-      flower.paste(todays_menu)
-    else
-      flower.paste(menu)
-    end
+    flower.paste(menu)
   end
 
   def self.menu
     document = Nokogiri.HTML(Typhoeus::Request.get(URL, :follow_location => true).body)
     document.at_css("#mainDiv > .post").css("p").map(&:text)[1..-1].join("\n")
-  end
-
-  def self.todays_menu
-    document = Nokogiri.HTML(Typhoeus::Request.get(URL, :follow_location => true).body)
-    document.at_css("#mainDiv > .post").css("p").map(&:text)[1..-1][WDAY..WDAY + 1].join("\n")
   end
 end
