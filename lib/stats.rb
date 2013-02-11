cbce08208507671f7e271ea8ec4897b604fec89f
Martin Svalin <martin.svalin@mynewsdesk.com>
2013-02-11 09:13:47 +0100

    Typo

--- lib/stats.rb
+++ lib/stats.rb
@@ -54,7 +54,7 @@ class Flower::Stats
       "Why the sad face, #{nick}?",
       "You are not looking too good #{nick}.",
       "Loooser #{nick}!",
-      "You are loosing it, #{nick}!",
+      "You are losing it, #{nick}!",
       "SAY SOMETHING #{nick}!?",
       "Are you dead #{nick}?"
     ].sample
