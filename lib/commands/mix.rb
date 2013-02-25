fab20dc546b140a6513accd51e76359691894496
hilla duka and Joakim Westerlund <dev+hilla+joakim@mynewsdesk.com>
2013-02-25 13:13:45 +0100

    Ooops

--- lib/commands/mix.rb
+++ lib/commands/mix.rb
@@ -3,6 +3,8 @@ require_relative 'sound_command'
 class Mix < SoundCommand
   respond_to "mix"
 
+  FILES = Dir.glob("extras/mix/*.mp3").map{|f| f.gsub("extras/","")}
+
   def self.description
     "In the mix, play with the music played by the bot!"
   end
