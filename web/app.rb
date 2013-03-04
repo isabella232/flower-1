93a6c2f5eb3413cb7d439ef3f494d7f314caed15
Martin Svalin <martin.svalin@mynewsdesk.com>
2013-03-04 15:28:49 +0100

    Basic soundboard

--- web/app.rb
+++ web/app.rb
@@ -7,3 +7,35 @@ end
 get '/' do
   erb :index
 end
+
+post '/sound' do
+  command = params[:command].gsub(/^!/, '')
+  message = params.fetch(:message, "")
+
+  respond(command, message) if command_exists?(command)
+  redirect '/'
+end
+
+def command_exists?(command)
+  Flower::COMMANDS.has_key?(command)
+end
+
+def respond(command, message)
+  Flower::COMMANDS[command].respond(command, message, sender, flower)
+end
+
+def flower
+  @flower ||= Flower::Web
+end
+
+def sender
+  {id: 0, nick: "flower"}
+end
+
+class Flower::Web
+  def self.say(message, mention = nil)
+  end
+
+  def self.paste(message, mention = nil)
+  end
+end
