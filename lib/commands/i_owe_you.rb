0a6c655b8f548ae40e1871b9e3daf0a8cd536f0d
Markus Nordin + Teo Ljungberg <dev+markus+teo@mynewsdesk.com>
2013-05-28 13:22:36 +0200

    More pretty output from !iou
    - !iou without receiver returns usage instructions

--- lib/commands/i_owe_you.rb
+++ lib/commands/i_owe_you.rb
@@ -11,18 +11,28 @@ class IOweYou < Flower::Command
 
     def respond message
       sender = message.sender[:nick].downcase
-      receiver = message.argument.split(" ").first.downcase
-      amount = message.argument.split(" ")[1]
+      begin
+        receiver = message.argument.split(" ").first.downcase
+        amount = message.argument.split(" ")[1]
+      rescue NoMethodError
+        return message.paste "Usage: !iou [name] ([amount])"
+      end
       debt = ::IOU::Debt.new(sender: sender, receiver: receiver, amount: amount.to_i)
 
       if receiver && amount
         if debt.create!
-          message.paste "Debt to #{receiver.capitalize} registered for #{amount} SEK, total debt: #{debt.previous_value}"
+          message.paste "Debt to #{receiver.capitalize} registered for #{amount} SEK. Total debt: #{debt.previous_value} SEK"
         else
           "You fail, sir!"
         end
       elsif receiver
-        message.paste "You owe #{debt.previous_value} SEK to #{receiver.capitalize}"
+        if debt.previous_value > 0
+          message.paste "You owe #{debt.previous_value} SEK to #{receiver.capitalize}"
+        elsif debt.previous_value < 0
+          message.paste "#{receiver.capitalize} owes you #{debt.previous_value} SEK"
+        else
+          message.paste "No debts between you and #{receiver.capitalize}!"
+        end
       end
     end
   end
