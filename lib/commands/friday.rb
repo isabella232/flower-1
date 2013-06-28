0cd5c76e264a5930312fad28861ee4496059aa25
David Forsberg <neversaydavid@gmail.com>
2013-06-28 14:52:09 +0200

    Lagt till en torsdag

--- lib/commands/friday.rb
+++ lib/commands/friday.rb
@@ -2,6 +2,7 @@
 require_relative 'sound_command'
 class Friday < SoundCommand
   respond_to "friday"
+  respond_to "thursday"
 
   def self.description
     "We, we, we so excited!"
@@ -10,6 +11,8 @@ class Friday < SoundCommand
   def self.respond(message)
     if Time.now.wday == 5
       play_file("friday/friday.mp3")
+    elsif Time.now.wday == 4
+      play_file("friday/thursday.mp3")
     else
       message.say("Today is not a Rebecca Black day", :mention => message.user_id)
     end
