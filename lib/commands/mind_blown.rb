af5cd03b6891d82f3769b007125784459ec94561
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-10-20 20:51:16 +0200

    There can only be one mind blown

--- lib/commands/mind_blown.rb
+++ lib/commands/mind_blown.rb
@@ -2,20 +2,11 @@
 class MindBlown < Flower::Command
   respond_to "mindblown"
 
-  MINDBLOWN = %w[
-    http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif
-    http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif
-    http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif
-    http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif
-    http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif
-    http://palmettopublicrecord.org/wp-content/uploads/2012/12/Kevin-Butler-Mind-Blown.gif
-  ]
-
   def self.description
     "Mind blown!"
   end
 
   def self.respond(message)
-    message.say MINDBLOWN.sample
+    message.say 'http://cdn.ebaumsworld.com/mediaFiles/picture/861722/82704856.gif'
   end
 end
