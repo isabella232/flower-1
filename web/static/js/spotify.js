69621f56a6ee3cd62f89c945065564f14c6e4943
Joakim Westerlund <joakim.westerlund@gmail.com>
2013-11-17 22:10:04 +0100

    Use requires to load scripts

--- web/static/js/spotify.js
+++ web/static/js/spotify.js
@@ -1,38 +1,41 @@
-function search(trackName){
-    var trackName = encodeURIComponent(trackName);
-    $.get("http://ws.spotify.com/search/1/track.json?q="+trackName,function(data){
-        var html = "";
-        var tracks = data.tracks;
-        $(tracks).each(function(i,elm){
-            var track = $(elm)[0];
-            var artists = "";
-            $(track.artists).each(function(i,item){
-                artists += item.name + " ";
+/*global jQuery:true, define:true */
+define(function() {
+    function search(trackName){
+        var trackName = encodeURIComponent(trackName);
+        $.get("http://ws.spotify.com/search/1/track.json?q="+trackName,function(data){
+            var html = "";
+            var tracks = data.tracks;
+            $(tracks).each(function(i,elm){
+                var track = $(elm)[0];
+                var artists = "";
+                $(track.artists).each(function(i,item){
+                    artists += item.name + " ";
+                });
+                html += "<li><span class=\"pop\">"+ parseInt(track.popularity * 100,10) +"</span><i class=\"fa fa-music\"></i> <span>"+ track.name + "</span> ("+ track.album.released +")<br><span>"+ artists +"</span><br><small>Length: "+ track.length +"</small> <a href=# data-track="+ track.href +">Load</a></li>";
             });
-            html += "<li><span class=\"pop\">"+ parseInt(track.popularity * 100,10) +"</span><i class=\"fa fa-music\"></i> <span>"+ track.name + "</span> ("+ track.album.released +")<br><span>"+ artists +"</span><br><small>Length: "+ track.length +"</small> <a href=# data-track="+ track.href +">Load</a></li>";
-        });
-        $("ul#result").html(html);
-        $("ul#result a").on("click",function(evt){
-            evt.preventDefault();
-            //console.log($(this).data("track"));
-            $.post("/spotify/",{
-                data:{track: $(this).data("track")}
+            $("ul#result").html(html);
+            $("ul#result a").on("click",function(evt){
+                evt.preventDefault();
+                //console.log($(this).data("track"));
+                $.post("/spotify/",{
+                    data:{track: $(this).data("track")}
+                });
             });
-        });
 
-    });
-}
-function setFilter(filter){
-    var filterSearch = filter + $("#track").val();
-    $("#track").val(filterSearch);
-    search(filterSearch);
-};
-$(function(){
-    $("input[type=text]").on("keyup",function(){
-        search($("#track").val());
-    });
-    $("a[data-filter]").on("click",function(){
-        event.preventDefault();
-        setFilter($(this).data("filter"));
+        });
+    }
+    function setFilter(filter){
+        var filterSearch = filter + $("#track").val();
+        $("#track").val(filterSearch);
+        search(filterSearch);
+    };
+    $(function(){
+        $("input[type=text]").on("keyup",function(){
+            search($("#track").val());
+        });
+        $("a[data-filter]").on("click",function(){
+            event.preventDefault();
+            setFilter($(this).data("filter"));
+        });
     });
 });
