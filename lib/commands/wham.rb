38e4e113b08969b08f7bdd4b64c19f14b2a8e346
David Billskog <billskog@gmail.com>
2013-12-11 15:26:30 +0100

    Wham 2.0

--- lib/commands/wham.rb
+++ lib/commands/wham.rb
@@ -1,16 +1,19 @@
 class Wham < Flower::Command
-  respond_to "wham", "wham!"
+  respond_to "wham", "WHAM"
+
+  WHAM_URI = "spotify:track:0QPYn15U8IQHKcH2LDfrek"
 
   def self.description
     'Boom!'
   end
 
   def self.respond(message)
-    if message.command == "wham!"
-      track = SpotifyCommand.get_track "spotify:track:0QPYn15U8IQHKcH2LDfrek", "Billskog"
+    if message.command == "WHAM"
+      track = SpotifyCommand.get_track WHAM_URI, "Billskog"
       SpotifyCommand.play_track(track)
     else
-      SpotifyCommand.add_to_queue "spotify:track:0QPYn15U8IQHKcH2LDfrek", 'Billskog'
+      SpotifyCommand.add_to_queue WHAM_URI, 'Billskog'
     end
+    message.say("Hell yeah!")
   end
 end
